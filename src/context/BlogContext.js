eff5218e69cff2661905ca1ced8ed14eab29eac2
Darren Lunsford <dlunsford@zulily.com>
2020-07-05 22:06:22 -0400

    feat: all crud processes work

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,14 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+        if (blogPost.id === action.payload.id) {
+          return action.payload
+        } else {
+          return blogPost;
+        }
+      })
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id != action.payload);
     case 'add_blogpost':
@@ -19,18 +27,29 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
   return (title, content, cb) => {
     dispatch({ type: 'add_blogpost', payload: { title, content } });
-    cb();
+    if (cb) {
+      cb();
+    }
   }
 }
 
 const deleteBlogPost = (dispatch) => {
   return (id) => {
-    dispatch({ type: 'delete_blogpost', payload: id })
+    dispatch({ type: 'delete_blogpost', payload: id });
+  }
+}
+
+const editBlogPost = (dispatch) => {
+  return (id, title, content, cb) => {
+    dispatch({ type: 'edit_blogpost', payload: { id, title, content } });
+    if (cb) {
+      cb();
+    }
   }
 }
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
-  []
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: 'test post', content: 'test content', id: 1 }]
 );
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,46 +1,15 @@
-import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import React, { useContext } from 'react';
 import { Context } from '../context/BlogContext';
-// import { Feather } from '@expo/vector-icons';
-
+import BlogPostForm from '../components/BlogPostForm';
 
 const CreateScreen = ({ navigation }) => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
   const { addBlogPost } = useContext(Context);
 
-  return <View>
-    <Text style={styles.label}>Enter Title:</Text>
-    <TextInput style={styles.textInputStyle} value={title} onChangeText={text => setTitle(text)} />
-
-    <Text style={styles.label}>Enter Content:</Text>
-    <TextInput style={styles.textInputStyle} value={content} onChangeText={text => setContent(text)} />
-
-    <Button title="Add Blog Post"
-      onPress={() => addBlogPost(title, content, () => {
-        navigation.navigate('Index');
-      })} />
-
-  </View>
-}
-
-
-const styles = StyleSheet.create({
-  textInputStyle: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: '#000000',
-    marginHorizontal: 15,
-    padding: 10,
-    marginVertical: 10
-
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 10,
-    marginHorizontal: 15,
-    marginVertical: 10
-  }
-});
+  return (
+    <BlogPostForm onSubmit={(title, content) => {
+      addBlogPost(title, content, () => { navigation.navigate('Index') })
+    }} />
+  );
+};
 
 export default CreateScreen;
