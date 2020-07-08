286694ac259cefb57ac9f7dec12c55f12d374cd4
Darren Lunsford <dlunsford@zulily.com>
2020-07-07 21:34:50 -0700

    delete: removed api files

--- src/api/jsonSerer.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'http://89d267c27b8d.ngrok.io'
-});
--- src/api/mofoNews.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://www.mofo.com/templates/content-wss?wssid=3333111'
-});
